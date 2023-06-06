d5f30e0f1970b248043181f41c7d29702d3b0033
bombizombi <davorin.mestric@gmail.com>
2023-06-06 12:29:54 +0200

    started text game

--- EmpireRUI/MainMenuViewModel.cs
+++ EmpireRUI/MainMenuViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using ReactiveUI;
+using System.Reactive;
 
 namespace EmpireRUI;
 
 public class MainMenuViewModel : ReactiveObject, IRoutableViewModel
 {
+    private ReactiveCommand<Unit, Unit> _startGameCommand;
     public MainMenuViewModel( IScreen screen)
     {
         HostScreen = screen;
+
+        _startGameCommand = ReactiveCommand.Create(() => HostScreen.Router.Navigate.Execute(new MapViewModel(HostScreen)));
+
+    }
+
+    private void StartGame()
+    {
+        var theGame = new EmpireTheGame(); //TODO: inject this
+        //game parameters are read from this main menu?
+
+        HostScreen.Router.Navigate.Execute(new MapViewModel(HostScreen));
     }
 
     public IScreen HostScreen { get; }
---
+++ EmpireRUI/Views/MapView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmpireRUI;
+
+/// <summary>
+/// Interaction logic for MapView.xaml
+/// </summary>
+public partial class MapView : MapViewBase
+{
+    public MapView()
+    {
+        InitializeComponent();
+    }
+}
+
+//workaround for XAML inabillity to inherit generic class
+public class MapViewBase : ReactiveUserControl<MapViewModel> { }
